de48944f179a0c53e9e71c2a937582eff86940ee
Rai Maciel <45213677+bizerratron@users.noreply.github.com>
2019-04-15 13:21:19 -0300

    Acabar depois

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,36 +2,40 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var arrayooo = ['Rinha de Galo', 'Galo de Calça', 'Tigresa Vip', 'Super Índio', 'Cerol on hand'];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+var fuckao = function(array){
+  return array;
+};
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
-
+fucako(arrayooo[1]);
+'Galo de Calça'
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
 segundo, um número. A função deve retornar o valor de um índice do array que foi passado
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+var queriaSerRico = function(array, num){
+  return array[num];
+ };
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var animais = ['Urso', 'Peixe', 'Cachorro', 'Super Índio', 'Gato'];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+fuckao(animais);
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,12 +51,31 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+var book = function(nomeLivro){
+  var objeto = {
+      world_of_warcraft_a_ruptura: {
+      quantidadePaginas: 336,
+      autor: 'Christie Golden',
+      editora: 'Galera Record'
+    },
+      world_of_warcraft_mares_da_guerra: {
+      quantidadePaginas: 350,
+      autor: 'Christie Golden',
+      editora: 'Galera Record'
+    },
+      world_of_warcraft_sombras_da_horda: {
+      quantidadePaginas: 294,
+      autor: 'Christie Golden',
+      editora: 'Galera Record'
+    }
+  };
+  return objeto;
+};
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(fuckao(book()));
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
